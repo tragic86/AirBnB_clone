ae71fcd94f85b967a9cdd19868f5aeb67be879fb
Madison Burke <madisononealburke@gmail.com>
2018-06-08 15:34:50 -0700

    add documentation

--- console.py
+++ console.py
@@ -1,8 +1,11 @@
 #!/usr/bin.python3
+""" Admin console for Holberton BnB project """
 import cmd, sys, shlex
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
+	''' Command Line interface for HBNB '''
 	prompt = '(hbnb) '
 
 	def do_echo(self, arg):
--- models/base_model.py
+++ models/base_model.py
@@ -13,15 +13,35 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        print("{}{}{}".format(self.id, self.created_at, self.updated_at))
-    def save(self):
-        pass
+        print()
+        print("id: " + self.id)
+        print("Created at " + str(self.created_at))
+        print("Updated at " + str(self.updated_at))
+        print()
+        print(type(self.updated_at))
+        print()
 
     def to_dict(self):
-        pass
+        self.__dict__['updated_at'] = str(self.updated_at.isoformat())
+        self.__dict__['created_at'] = str(self.created_at.isoformat())
+        self.__dict__['__class__'] = self.__class__.__name__
+        return self.__dict__
+
+#       @updated_at.setter
+#       def save(self):
+#           self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """override the string displayed by __str__ method"""
         return("[{}] ({}) {}".format
                 (self.__class__.__name__, self.id,
                  self.__dict__))
+
+
+
+def test():
+    h = Base()
+    print(h)
+    print()
+if __name__ == '__main__':
+    test()
