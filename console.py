18d7be5471c27a33f0892405157ebfd1ff7a44b6
Madison Burke <madisononealburke@gmail.com>
2018-06-11 13:36:12 -0700

    subclasses

--- console.py
+++ console.py
@@ -1,8 +1,13 @@
 #!/usr/bin.python3
 """ Admin console for Holberton BnB project """
 import cmd, sys, shlex
-from models.base_model import BaseModel
+from models.user import User
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import json
 
 
