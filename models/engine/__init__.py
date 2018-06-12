ed5988d782c4f52e8c48c6ef7cc3c3a3ca435076
AmyLGalles <suzyhendrix@gmail.com>
2018-06-12 20:24:00 +0000

    pep8 checked init, removed some notes

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -2,7 +2,3 @@ from .file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
-
-''' when multiple files initialize the 
-engine directory, will the code above initialize
-multiple times? '''
