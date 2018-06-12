b5da595733b14cdc259cc28d1c828a9bdcdacd20
AmyLGalles <suzyhendrix@gmail.com>
2018-06-12 20:28:12 +0000

    pep8 checked file_storage, make sure this is most current version of code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,22 +1,47 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import json
+
 """File Storage Module
 
-   This modules will allow us to 
+   This modules will allow us to
    preserve our class instances into a data store
 """
 
 
 class FileStorage:
-	__file_path = "file.json"
-	__objects = {}
+    """handles serialization and deserialization of BaseModel class objects"""
+    __file_path = "file.json"
+    __objects = {}
 
-	def reload(self):
-		pass
+    def reload(self):
+        """ returns the dictionary __objects
+        try/except with file not found error -> pass
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                dicts = json.load(f)
+            for key, value in dicts.items():
+                obj1 = eval(value['__class__'])(**value)
+                self.__objects[key] = obj1
+        except FileNotFoundError:
+            pass
 
-	def all(self):
-		pass
+    def all(self):
+        """ sets in __objects the obj with key <obj class name>.id """
+        return self.__objects
 
-	def save(self):
-		pass
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
+        new_dict = {}
+        if (self.__objects):
+            for key, value in self.__objects.items():
+                new_dict[key] = value.to_dict()
+            with open(self.__file_path, 'w+') as f:
+                json.dump(new_dict, f)
 
-	def new(self):
-		pass
+    def new(self, obj):
+        """ deserializes the JSON file to __objects (only if the JSON file
+        exists ; otherwise, do nothing) """
+        new_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[new_key] = obj
