00b73e51dc311fe434596b14d3f63031389cb67b
AmyLGalles <suzyhendrix@gmail.com>
2018-06-13 19:59:59 +0000

    Removed some redundant and inefficient code

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,7 @@ class BaseModel:
         """init method for base_model"""
         if not (kwargs):
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
         else:
